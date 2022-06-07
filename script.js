3a5f1556216b6d1eee9330dbe868d124708ebec2
Seb <sbstn1@gmail.com>
2022-06-07 18:58:38 -0300

    cool thing

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 var scr = document.getElementById("hex");
 var hex = scr.getContext("2d");
-
-hex.fillStyle = "WhiteSmoke";
-hex.fillRect(0,0,600,400);
-hex.strokeStyle = "black";
 hex.beginPath();
+
+
 /*
 hex.moveTo(100,100);
 hex.lineTo(200+16,100);
@@ -17,27 +15,67 @@ hex.lineTo(100,100);
 hex.stroke();
 */
 
-function randomHexLine(x, y, count, size){
+function randomHexLine(x, y, count, size, color){
     var i = count;
+    var arrayX = [1, 0.5, -0.5, -1, -0.5, 0.5];
+    var arrayY = [0, -0.866, -0.866, 0, 0.866, 0.866];
     var direction = Math.floor(Math.random()*6);
+    hex.strokeStyle = color;
+    hex.beginPath();
+
     hex.moveTo(x,y);
-    var posX = x + Math.sin(direction)*size;
-    var posY = y + Math.cos(direction)*size;
+    var posX = x + arrayX[direction]*size;
+    var posY = y + arrayY[direction]*size;
 
     hex.lineTo(posX, posY);
+    var path = 0;
 
     for(var i=0; i<count; i++){
-        var dir = Math.round(Math.random());
-        if(dir){
+        
+        if(Math.round(Math.random())){
             direction++;
+            path++;
         }else{
             direction--;
+            path--;
         }
-        posX += Math.sin(direction)*size;
-        posY += Math.cos(direction)*size;
+        
+        if(path>3){
+            direction-=2;
+            path-=2;
+        }
+        if(path<-3){
+            direction+=2;
+            path+=2;
+        }
+
+        if(direction>5){
+            direction=0;
+        }else if(direction<0){
+            direction=5;
+        }
+        posX += arrayX[direction]*size;
+        posY += arrayY[direction]*size;
         hex.lineTo(posX, posY);
     }
-    hex.stroke();
+    hex.stroke();    
+
+    return [posX, posY];
 }
 
-randomHexLine(300,200, 20, 20);
+var colors = ["green","blue","red","cyan","grey","pink","yellow","orange","brown"];
+var pos = [300,200];
+
+
+scr.addEventListener("mousemove", (e)=>{
+    let rect = e.target.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+    hex.fillStyle = "black";
+    hex.fillRect(0,0,1000,800);
+
+    for(var i=0; i<colors.length; i++){
+        randomHexLine(x, y, 10, 20, colors[i]);
+        console.log(x, y);
+    }
+});
