aec80167d62fdc0e2df91115022bfe232bdd4a9e
Seb <sbstn1@gmail.com>
2022-06-08 01:45:54 -0300

    se va a estabilizar!

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
 var scr = document.getElementById("hex");
 var hex = scr.getContext("2d");
 hex.beginPath();
+hex.fillStyle = "black";
+hex.fillRect(0,0,1000,800);
 
-
-/*
-hex.moveTo(100,100);
-hex.lineTo(200+16,100);
-hex.lineTo(260+16,200);
-hex.lineTo(200+16,300);
-hex.lineTo(100,300);
-hex.lineTo( 40,200);
-hex.lineTo(100,100);
-
-hex.stroke();
-*/
-
-function randomHexLine(x, y, count, size, color){
+function randomHexLine(x, y, count, size, color, type){
     var i = count;
     var arrayX = [1, 0.5, -0.5, -1, -0.5, 0.5];
     var arrayY = [0, -0.866, -0.866, 0, 0.866, 0.866];
-    var direction = Math.floor(Math.random()*6);
+    var direction = Math.floor(Math.random()*3);
+    if(type){
+        direction = direction*2;
+    }else{
+        direction = direction*2+1;
+    }
     hex.strokeStyle = color;
     hex.beginPath();
 
@@ -28,27 +22,14 @@ function randomHexLine(x, y, count, size, color){
     var posY = y + arrayY[direction]*size;
 
     hex.lineTo(posX, posY);
-    var path = 0;
 
     for(var i=0; i<count; i++){
         
         if(Math.round(Math.random())){
             direction++;
-            path++;
         }else{
             direction--;
-            path--;
         }
-        
-        if(path>3){
-            direction-=2;
-            path-=2;
-        }
-        if(path<-3){
-            direction+=2;
-            path+=2;
-        }
-
         if(direction>5){
             direction=0;
         }else if(direction<0){
@@ -59,13 +40,9 @@ function randomHexLine(x, y, count, size, color){
         hex.lineTo(posX, posY);
     }
     hex.stroke();    
-
-    return [posX, posY];
 }
 
 var colors = ["green","blue","red","cyan","grey","pink","yellow","orange","brown"];
-var pos = [300,200];
-
 
 scr.addEventListener("mousemove", (e)=>{
     let rect = e.target.getBoundingClientRect();
@@ -75,7 +52,15 @@ scr.addEventListener("mousemove", (e)=>{
     hex.fillRect(0,0,1000,800);
 
     for(var i=0; i<colors.length; i++){
-        randomHexLine(x, y, 10, 20, colors[i]);
-        console.log(x, y);
+        var size = 30;
+        randomHexLine(x-x%(size*0.5*1.5), y-y%(size*0.866*2), 10, size, colors[i], hexType(x, y, size));
     }
 });
+
+function hexType(x, y, size){
+    if(Math.floor((x%(size*0.5*1.5))/size) === Math.floor((y%(size*0.866*2))/size)){
+        return true;
+    }else{
+        return false;
+    }
+}
